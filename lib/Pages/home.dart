4529fe6129f89e61ec4f77a2979733155a6dcf03
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-12-30 11:54:46 +0530

    added leading button in the app bar

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,12 +10,18 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           "Breakfast",
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 18
-          ),
+          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
         ),
         centerTitle: true,
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: Colors.black12, borderRadius: BorderRadius.circular(10)),
+          child: const Icon(
+            Icons.arrow_back_ios_new_sharp,
+            size: 20,
+          ),
+        ),
       ),
     );
   }
