ccdaa0bffdc7db8c4ed27d3c700d116978ba2b67
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2025-01-12 16:59:21 +0530

    added ategories method to a component

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:fitness/components/appBar.dart';
+import 'package:fitness/components/categories.dart';
 import 'package:fitness/components/searchBar.dart';
 import 'package:fitness/models/category_model.dart';
 import 'package:flutter/material.dart';
@@ -40,72 +41,11 @@ class _HomeState extends State<Home> {
           const SizedBox(
             height: 40,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  "Category",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w800),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                height: 120,
-                child: ListView.separated(
-                  itemCount: categories.length,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.only(
-                    left: 20,
-                    right: 20
-                  ),
-                  separatorBuilder: (context, index) => const SizedBox(width: 25,),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: categories[index].boxColor.withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(16)
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle
-                            ),
-                            child: SvgPicture.asset(categories[index].iconPath),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            categories[index].name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                              fontSize: 14
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              )
-            ],
-          )
+          Categories(categories)
         ],
       ),
     );
   }
+
+ 
 }
